5d3ffa483b29c817d0654215a8ac1f55b6fb5f43
Hussny Azzam <16493099+HussnyAzzam@users.noreply.github.com>
2026-07-18 23:44:08 +0300

    Add redirect for root path to home.html
    Redirect to home.html when on the root path.

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Routes, Route, useNavigate, useLocation } from 'react-router-dom';
 import MainPage from './components/MainPage/MainPage';
 import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
@@ -9,21 +9,31 @@ import Profile from './components/Profile/Profile';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+
 function App() {
   const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname === '/') {
+      window.location.href = '/home.html';
+    }
+  }, [location]);
+
   return (
-        <>
-        <Navbar/>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/app" element={<MainPage />} />
-          <Route path="/app/login" element={<LoginPage/>} />
-          <Route path="/app/register" element={<RegisterPage />} />
-          <Route path="/app/product/:productId" element={<DetailsPage/>} />
-          <Route path="/app/search" element={<SearchPage/>} />
-          <Route path="/app/profile" element={<Profile/>} />
-        </Routes>
-        </>
+    <>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={null} /> 
+        <Route path="/app" element={<MainPage />} />
+        <Route path="/app/login" element={<LoginPage/>} />
+        <Route path="/app/register" element={<RegisterPage />} />
+        <Route path="/app/product/:productId" element={<DetailsPage/>} />
+        <Route path="/app/search" element={<SearchPage/>} />
+        <Route path="/app/profile" element={<Profile/>} />
+      </Routes>
+    </>
   );
 }
+
 export default App;
